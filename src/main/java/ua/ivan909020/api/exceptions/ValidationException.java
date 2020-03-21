2f0836cab69d20ef689a085ec463de34634d47c5
Ivan Zaitsev <ivan909020@gmail.com>
2020-03-21 17:20:36 +0200

    added custom exceptions

---
+++ src/main/java/ua/ivan909020/api/exceptions/ValidationException.java
@@ -0,0 +1,16 @@
+package ua.ivan909020.api.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ValidationException extends RuntimeException {
+
+	public ValidationException() {
+	}
+
+	public ValidationException(String message) {
+		super(message);
+	}
+
+}
